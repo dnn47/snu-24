7fdd351a7fff30226cc68b3b30c8c7100070a817
Dune <dnn47@drexel.edu>
2023-12-29 14:29:01 -0500

    tried differnt landing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -6,30 +6,19 @@ function Landing() {
   return (
     <>
       <div className="d-flex align-items-center justify-content-center vh-100">
-        {/* <h1 className="animation">Seoul National University</h1> */}
-        {/* <img className="logo animation" src={"src/images/logo.png"} alt="SNU Logo" /> */}
-
-        <TypeAnimation
-          sequence={[
-            "Seoul Stories: Neon Nights",
-            1000,
-            "Seoul Stories: Soju Shenanigans",
-            1000,
-            "Seoul Stories: Lost",
-            1000,
-            "Seoul Stories: Taekwondo",
-            1000,
-            "Seoul Stories: Karaoke Nights",
-            1000,
-            "Seoul Stories: Culture Shock",
-            1000,
-          ]}
-          wrapper="span"
-          speed={{ type: "keyStrokeDelayInMs", value: 100 }}
-          deletionSpeed={10}
-          style={{ fontSize: "10vh", display: "inline-block" }}
-          repeat={Infinity}
-        />
+        <div className="container">
+          <h1>Study Aboard at:</h1>
+          <br />
+          <h1 className="text">Seoul National University</h1>
+          {/* <TypeAnimation
+            sequence={["Seoul National University"]}
+            wrapper="span"
+            speed={{ type: "keyStrokeDelayInMs", value: 100 }}
+            deletionSpeed={10}
+            style={{ fontSize: "1000%", display: "inline-block" }}
+            repeat={0}
+          /> */}
+        </div>
       </div>
     </>
   );
