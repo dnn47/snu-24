b36cb79d863ad8b0f1a3553bdd42d48b35f89a1a
Dune <dnn47@drexel.edu>
2023-12-29 14:59:59 -0500

    deployed latest landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../css/Landing.css";
-import { TypeAnimation } from "react-type-animation";
 
 function Landing() {
   return (
@@ -10,14 +9,6 @@ function Landing() {
           <h1>Study Aboard at:</h1>
           <br />
           <h1 className="text">Seoul National University</h1>
-          {/* <TypeAnimation
-            sequence={["Seoul National University"]}
-            wrapper="span"
-            speed={{ type: "keyStrokeDelayInMs", value: 100 }}
-            deletionSpeed={10}
-            style={{ fontSize: "1000%", display: "inline-block" }}
-            repeat={0}
-          /> */}
         </div>
       </div>
     </>
