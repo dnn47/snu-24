f3c99e8f7d0076111970b8d412c613637e1b113a
Dune <dnn47@drexel.edu>
2024-01-13 21:44:59 -0500

    added translate to all pages

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,37 +1,25 @@
 import React from "react";
 import "../css/Landing.css";
-import { TypeAnimation } from "react-type-animation";
 import NavBar from "./NavBar";
+import { translateThis } from "../utils/TranslateServices";
+import { useTitleAnimation } from "../utils/TitleAnimation";
 
 function Landing() {
+  const typeAnimation = useTitleAnimation(
+    "hi, i'm Dune\ni studied abroad at SNU\nthis is my story"
+  );
   return (
     <>
-      <div className="container">
+      <div className="container p-5">
         <div className="row custom-height"></div>
-        <div className="row">
-          <TypeAnimation
-            className="text-title text-left p-5"
-            sequence={[
-              "hi, i'm dune\ni studied abroad at SNU\nthis is my story",
-            ]}
-            wrapper="span"
-            speed={{ type: "keyStrokeDelayInMs", value: 100 }}
-            deletionSpeed={10}
-            style={{
-              whiteSpace: "pre",
-            }}
-            repeat={0}
-          />
-        </div>
+        <div className="row">{typeAnimation}</div>
       </div>
-      <div>
-        <NavBar
-          nameOne="About Me"
-          linkOne="about"
-          nameTwo="Gilman Scholarship"
-          linkTwo="gilman"
-        />
-      </div>
+      <NavBar
+        nameOne={translateThis("About Me").String}
+        linkOne="about"
+        nameTwo={translateThis("Gilman Scholarship").String}
+        linkTwo="gilman"
+      />
     </>
   );
 }
