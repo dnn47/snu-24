44f37bacd632d089e3773ae39ad66741d837e7ab
Dune <dnn47@drexel.edu>
2024-07-07 22:42:17 -0400

    added texts

--- src/texts/test.tsx
+++ src/texts/test.tsx
@@ -1,21 +1,18 @@
 const textContent = [
-  `
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a enim
-  tempus, commodo tortor tincidunt, gravida lacus. Aliquam erat
-  volutpat. Donec rutrum aliquam interdum. Fusce ipsum magna, molestie
-  volutpat malesuada dapibus, blandit at mi. Phasellus nec quam
-  ultrices turpis rutrum dignissim. Donec placerat faucibus mauris, a
-  posuere enim pharetra id. In hac habitasse platea dictumst. Praesent
-  dolor ligula, vulputate id commodo id, pellentesque ac justo. Duis
-  porttitor pellentesque leo, maximus venenatis ipsum mollis eu.
-  Suspendisse consequat nunc eu nunc suscipit, id vestibulum neque
-  dictum. Pellentesque habitant morbi tristique senectus et netus et
-  malesuada fames ac turpis egestas. Vivamus venenatis lacus nec neque
-  ultrices mollis. Morbi sed vehicula velit, a molestie enim.
-  Vestibulum a sagittis magna, id posuere dolor. Curabitur condimentum
-  congue justo.
-`,
-  `poop`,
+  `SNU ORIENTATION.`,
+  `CAT CAFE`,
+  `KOREA COSTCO`,
+  `NAMSAN SEOUL TOWER`,
+  `BUSAN`,
+  `FOOD FESTIVAL`,
+  `GWANKGU MOUTAIN`,
+  `DOG CAFE`,
+  `SOKCHO`,
+  `BASEBALL GAME`,
+  `SNU BUDDY MT GROUP PICS`,
+  `BIG GROUP MT`,
+  `JEJU`,
+  `FAREWELL DINNER`,
 ];
 
 export default textContent;
