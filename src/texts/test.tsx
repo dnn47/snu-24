2b998b133a85202cf904ace7f194df4110dacbf9
Dune <dnn47@drexel.edu>
2024-07-15 19:00:31 -0400

    week 1 text

--- src/texts/test.tsx
+++ src/texts/test.tsx
@@ -1,5 +1,5 @@
 const textContent = [
-  `SNU ORIENTATION.`,
+  `My first week in Korea was a whirlwind of emotions and experiences. I arrived not knowing the language and sharing a room with three strangers, my roommates, whom I had met just a week prior. Initially, I felt quite alone and isolated, unable to understand anything around me. However, as the week went on, I bonded with my roommates over our shared awe and confusion of Korea. This photo captures my roommates, friends, and me at the Seoul National University International Student Orientation. This marks the beginning of my adventure.`,
   `CAT CAFE`,
   `KOREA COSTCO`,
   `NAMSAN SEOUL TOWER`,
